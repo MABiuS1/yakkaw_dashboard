9e246b4f51e6d3d3837eb7b1b3fd0aa0321467de
frank1nz <151651409+frank1nz@users.noreply.github.com>
2025-08-21 16:46:37 +0700

    Delete ClientLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import ClientLayout from '@/app/ClientLayout'; // 👈 import ที่เราเพิ่งสร้าง
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,9 +15,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} min-h-screen bg-background font-sans antialiased`}>
-        <ClientLayout> {/* 👈 wrap children ด้วย ClientLayout */}
+      
           {children}
-        </ClientLayout>
       </body>
     </html>
   );
